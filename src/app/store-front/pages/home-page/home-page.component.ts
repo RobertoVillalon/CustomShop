6b02d54dd34b5e2c2417fc8c898d0c7151d95b34
RobertoVillalon <robertoantonio3756@gmail.com>
2025-08-25 11:29:06 -0400

    Se generan modulos, configuracion de animaciones y fuentes, y se agregan productos

---
+++ src/app/store-front/pages/home-page/home-page.component.ts
@@ -0,0 +1,10 @@
+import { ProductCardComponent } from '@/product/components/product-card/product-card.component';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-home-page',
+  imports: [ProductCardComponent],
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent { }
